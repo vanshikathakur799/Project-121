b5cc917eced2c86c3cf5dd13880aef20760ff14d
VanshikaThakur <103041940+vanshikathakur799@users.noreply.github.com>
2023-05-04 12:42:53 +0530

    Add files via upload

--- sign_language.py
+++ sign_language.py
@@ -24,28 +24,28 @@ while True:
                 lm_list.append(lm)
 
              #Code goes here   
-            finger_tips = [8, 12, 16,20]
-            for tip in finger_tips:
-                x, y = int(lm_list[tip].x*w), int(lm_list[tip].y*h)
-                cv2.circle(img, (x, y), 15, (255, 0, 0), cv2.FILLED)
+        finger_tips = [8, 12, 16,20]
+        for tip in finger_tips:
+            x, y = int(lm_list[tip].x*w), int(lm_list[tip].y*h)
+            cv2.circle(img, (x, y), 15, (255, 0, 0), cv2.FILLED)
 
-            finger_fold_status = []    
+        finger_fold_status = []    
 
-            if lm_list[tip].x < lm_list[tip - 3].x:
-                cv2.circle(img, (x, y), 15, (0, 255, 0), cv2.FILLED)
-                finger_fold_status.append(True)
+        if lm_list[tip].x < lm_list[tip - 3].x:
+            cv2.circle(img, (x, y), 15, (0, 255, 0), cv2.FILLED)
+            finger_fold_status.append(True)
 
-            else:
-                finger_fold_status.append(False)    
+        else:
+            finger_fold_status.append(False)    
 
-            if all(finger_fold_status):
-                if lm_list[thumb_tip].y < lm_list[thumb_tip - 1].y < lm_list[thumb_tip - 2].y:
-                    print("LIKE")
-                    cv2.putText(img, "LIKE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)    
+        if all(finger_fold_status):
+            if lm_list[thumb_tip].y < lm_list[thumb_tip - 1].y < lm_list[thumb_tip - 2].y:
+                print("LIKE")
+                cv2.putText(img, "LIKE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)    
 
-                if lm_list[thumb_tip].y > lm_list[thumb_tip - 1].y > lm_list[thumb_tip - 2].y:
-                    print("DISLIKE")
-                    cv2.putText(img, "DISLIKE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)    
+            if lm_list[thumb_tip].y > lm_list[thumb_tip - 1].y > lm_list[thumb_tip - 2].y:
+                print("DISLIKE")
+                cv2.putText(img, "DISLIKE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)    
 
 
             mp_draw.draw_landmarks(img, hand_landmark,
@@ -54,5 +54,7 @@ while True:
     
 
     cv2.imshow("hand tracking", img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == 32:
+        break
     
